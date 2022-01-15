cf4f4fc987390f7c3d01f08ad5bedb7adc640c7e
kerunix <pierre@kissmy.co>
2022-01-15 20:11:38 +0100

    with bug

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+// Swap the commented line to trigger the bug on and off
+// const { data } = useAsyncData('count', () => $fetch('/api/helloWorld'))
+const { data } = useAsyncData('count', () => $fetch('/api/hello-world'))
+</script>
+
+
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
